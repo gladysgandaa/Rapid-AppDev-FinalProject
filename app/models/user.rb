86d5aeec4395270622748b838503ea5783a31153
Gladys Ganda <s3673989@student.rmit.edu.au>
2020-05-02 15:12:37 +0000

    Regex updated and UI for user settings created

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :username, presence: true, uniqueness: true
+  VALID_MOBILE_REGEX = /\A[0-9]{10,13}\z/
+  validates :mobile, presence: true, format: { with: VALID_MOBILE_REGEX }, uniqueness: true
+  VALID_PASSWORD_REGEX = /\A([a-zA-Z0-9]{8,20})\z/
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
+end
