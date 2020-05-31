9683c218d9efdaf90fb866acf3cf955901ced7be
s3679389 <s3679389@student.rmit.edu.au>
2020-05-31 15:13:33 +1000

    Add search bar and fix jquery issue

--- project/app/assets/javascripts/application.js
+++ project/app/assets/javascripts/application.js
@@ -10,67 +10,69 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
-//= require turbolinks
-//= require articles
 //= require jquery
 //= require jquery_ujs
+//= require activestorage
+//= require articles
+//= require turbolinks
 //= require settings.js
-//= require_tree .
 
-$( document ).on('turbolinks:load', function() {
-    console.log("It works on each visit!")
-  })
+jQuery(document).ready(function($){
+  $( document ).on('turbolinks:load', function() {
+      console.log("It works on each visit!")
+    })
 
-$(document).on('turbolinks:load', function() {
-    $("#SelectedPosts").hide();
+  $(document).on('turbolinks:load', function() {
+      $("#SelectedPosts").hide();
+    });
+
+  $(document).on('turbolinks:load', function() {
+    $("[data-js-hide-link]").click(function (event) {
+      $(this).parents("li").hide();
+      event.preventDefault();
+    });
   });
 
-$(document).on('turbolinks:load', function() {
-  $("[data-js-hide-link]").click(function (event) {
-    $(this).parents("li").hide();
-    event.preventDefault();
+    $(document).on('turbolinks:load', function() {
+      $("[data-js-toggle-topic]").click(function (event) {
+        console.log($("#"+ event.currentTarget.getAttribute("data-topic")))
+        $("." + event.currentTarget.getAttribute("data-topic")).toggle();    
+
+        if($("#"+ event.currentTarget.getAttribute("data-topic")).css('color')==='rgb(255, 165, 0)')
+        {
+          console.log($("#"+ event.currentTarget.getAttribute("data-topic")).css('color'))
+          console.log('if')
+          $("#"+ event.currentTarget.getAttribute("data-topic")).css({
+          'color':'rgb(0, 0, 0)'
+          })
+        }
+        else {
+          console.log('else')
+          $("#"+ event.currentTarget.getAttribute("data-topic")).css({
+            'color':'rgb(255, 165, 0)'
+          })
+        }   
+        console.log("hiding: ." + event.currentTarget.getAttribute("data-topic"));
+        event.preventDefault();
+      });
+    });
+
+
+  $(document).on('turbolinks:load', function() {
+    $("[data-js-selected-posts]").click(function (event) {
+      console.log("Selected Posts");
+      $("#SelectedPosts").show();
+      $("#RecentPosts").hide();
+      event.preventDefault();
+    });
   });
-});
 
-$(document).on('turbolinks:load', function() {
-  $("[data-js-toggle-topic]").click(function (event) {
-    console.log($("#"+ event.currentTarget.getAttribute("data-topic")))
-    $("." + event.currentTarget.getAttribute("data-topic")).toggle();    
-
-    if($("#"+ event.currentTarget.getAttribute("data-topic")).css('color')==='rgb(255, 165, 0)')
-    {
-      console.log($("#"+ event.currentTarget.getAttribute("data-topic")).css('color'))
-      console.log('if')
-      $("#"+ event.currentTarget.getAttribute("data-topic")).css({
-      'color':'rgb(0, 0, 0)'
-      })
-    }
-    else {
-      console.log('else')
-      $("#"+ event.currentTarget.getAttribute("data-topic")).css({
-        'color':'rgb(255, 165, 0)'
-      })
-    }   
-    console.log("hiding: ." + event.currentTarget.getAttribute("data-topic"));
-    event.preventDefault();
-  });
-});
-
-$(document).on('turbolinks:load', function() {
-  $("[data-js-selected-posts]").click(function (event) {
-    console.log("Selected Posts");
-    $("#SelectedPosts").show();
-    $("#RecentPosts").hide();
-    event.preventDefault();
-  });
-});
-
-$(document).on('turbolinks:load', function() {
-  $("[data-js-recent-posts]").click(function (event) {
-    console.log("Recent Posts");
-    $("#SelectedPosts").hide();
-    $("#RecentPosts").show();
-    event.preventDefault();
+  $(document).on('turbolinks:load', function() {
+    $("[data-js-recent-posts]").click(function (event) {
+      console.log("Recent Posts");
+      $("#SelectedPosts").hide();
+      $("#RecentPosts").show();
+      event.preventDefault();
+    });
   });
 });
--- project/config/environments/production.rb
+++ project/config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'assignment-rapid.herokuapp.com/'
+  host = 'rapid-ass.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
